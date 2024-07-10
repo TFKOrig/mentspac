5a4fbe3c67859dc8ca1548fcc28609179bc16093
bhatkhans7 <bhatkhans7@gmail.com>
2024-07-10 19:00:22 +0000

    changes done

--- components/Profilecard.tsx
+++ components/Profilecard.tsx
@@ -1,13 +1,22 @@
-import { Container } from "@mui/material";
+import { Box } from "@mui/material"
 
 export function Profilecard() {
   return (
-    <>
-      <Container fixed >
-        <h1>Hello World</h1>
-      </Container>
-    </>
-  );
+    <Box sx={{
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      Width:'60vw',
+      Height:'60vh',
+      margin:'0 auto',
+      backgroundImage: 'url(/images/HDimgback.jpg)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+    }}>
+      <h1>HI</h1>
+    </Box>
+  )
 }
 
 export default Profilecard
