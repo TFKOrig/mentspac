cb142a0509e99a8fdc5a77fbe40746fa21c14cbe
bhatkhans7 <bhatkhans7@gmail.com>
2024-07-12 14:18:45 +0000

    Made follownFollowing cards

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,12 @@ import { AppProps } from "next/app";
 import Profile from "../components/Postpage/Profilebgkandavatar";
 import Postnav from "../components/Postpage/Postnav";
 import Postcard from "../components/Postpage/Biocard";
+import Fnfcard from "../components/FollowersNFollowingpage/Fnfcard";
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   // return <Profile />;
   // return <Postnav />;
-  return <Postcard/>;
+  // return <Postcard/>;
+  return <Fnfcard />;
 };
 
 export default MyApp;
