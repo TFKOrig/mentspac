073ae99106bb9e3559b37ea648ea7cb287ba44b7
bhatkhans7 <bhatkhans7@gmail.com>
2024-07-12 14:54:26 +0000

    All done sent comps to client wll wait for further changes

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,11 +1,15 @@
-// import React from 'react';
+import React from "react";
+import Profilecard from "../components/Postpage/Profilebgkandavatar";
+import Postnav from "../components/Postpage/Postnav";
+import Postcard from "../components/Postpage/Biocard";
+import Fnfcard from "../components/FollowersNFollowingpage/Fnfcard";
+import Pnvcard from "../components/PicandVidpage/Pnvcard";
+const Header: React.FC = () => {
+  return (
+    <>
+      <Profilecard />
+    </>
+  );
+};
 
-// const Header: React.FC = () => {
-//   return (
-//     <header>
-//       <h1>My Next.js App</h1>
-//     </header>
-//   );
-// };
-
-// export default Header;
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,9 @@
 // pages/_app.tsx
 import React from "react";
 import { AppProps } from "next/app";
-import Profile from "../components/Postpage/Profilebgkandavatar";
-import Postnav from "../components/Postpage/Postnav";
-import Postcard from "../components/Postpage/Biocard";
-import Fnfcard from "../components/FollowersNFollowingpage/Fnfcard";
+import Profile from "../pages/Profile";
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  // return <Profile />;
-  // return <Postnav />;
-  // return <Postcard/>;
-  return <Fnfcard />;
+  return <Profile />;
 };
 
 export default MyApp;
